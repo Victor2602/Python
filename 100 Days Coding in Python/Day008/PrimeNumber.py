c628861f4e69e2279939625821854d63dc98fcf8
Victor Silva <vhns26@outlook.com>
2024-06-13 16:26:07 -0300

    PrimeNumber.py

---
+++ 100 Days Coding in Python/Day008/PrimeNumber.py
@@ -0,0 +1,11 @@
+# Write your code below this line 👇
+def prime_checker(number):
+    if number==1:
+      print("It's not a prime number.")
+    elif number==3:
+      print("It's a prime number.")
+    elif number%2==0 or number%3==0:
+      print("It's not a prime number.")
+    elif number%n == 0 and number%1==0:
+      print("It's a prime number.")
+
